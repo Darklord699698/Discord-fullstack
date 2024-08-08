b2d2d8df3c6f59640501a08b96cb9ad05e99a9da
UJJWAL <ujjwal8851114020@gmail.com>
2024-08-08 13:29:47 +0530

    1:30bje

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,7 +1,12 @@
-export default function Home() {
-  return (
+import { ModeToggle } from "@/components/mode-toggle";
+import { UserButton } from "@clerk/nextjs";
+
+export default function Home(){
+  return(
     <div>
-      This is a protected route
+      <UserButton  afterSignOutUrl="/" 
+      />
+     <ModeToggle/>
     </div>
-  );
+  )
 }
